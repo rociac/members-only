7d90d5c9c4bad89d962be5f5fcfee1e94b9b2a56
Adebola <onedebos@gmail.com>
2019-10-01 17:27:43 +0100

    Add authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,25 @@
 class User < ApplicationRecord
   has_secure_password
+  before_create :create_remember_digest 
+
+
+
+  class << self
+    
+    def digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    
+    BCrypt::Password.create(string, cost: cost)
+    end
+    
+    def new_token
+    SecureRandom.urlsafe_base64
+    end
+  end 
+  
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end 
+
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_152546) do
+ActiveRecord::Schema.define(version: 2019_10_01_160127) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2019_10_01_152546) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "auth_token"
+    t.string "remember_digest"
   end
 
 end
