f9ff6d0c416380475781df772a7430778628dae6
Adebola <onedebos@gmail.com>
2019-10-08 18:10:38 +0100

    Build sign up form

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -11,7 +11,7 @@ class MicropostsController < ApplicationController
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = "Micropost created!"
-      redirect_to root_url
+      redirect_to microposts_index_path
     else
       render 'static_pages/home'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   post 'microposts/new', to: 'microposts#create'
   get 'microposts/index'
   get    '/login',   to: 'sessions#new'
+  get '/signup', to: 'users#create'
+  post '/signup', to: 'users#create'
+  resources :users
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+  
 end
