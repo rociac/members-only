02b8d4f5f352491811905338e49c21b5885f22dd
rociac <brsrkdev@gmail.com>
2019-10-08 11:20:58 -0500

    Fix micropost association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :posts, dependent: :destroy
+  has_many :microposts, dependent: :destroy
 
   attr_accessor :remember_token
   before_save :downcase_email
