75641d2c6882c9d92b6c9f1c0168d602f1ec85f7
rociac <brsrkdev@gmail.com>
2019-10-08 12:48:59 -0500

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'microposts/new'
-  post 'microposts/new', to: 'microposts#create'
-  get 'microposts/index'
-  get    '/login',   to: 'sessions#new'
-  get '/signup', to: 'users#create'
-  post '/signup', to: 'users#create'
-  resources :users
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  root   'static_pages#home'
+  post   'microposts/new',    to: 'microposts#create'
+  get    '/login',            to: 'sessions#new'
+  get    '/signup',           to: 'users#create'
+  post   '/signup',           to: 'users#create'
+  post   '/login',            to: 'sessions#create'
+  delete '/logout',           to: 'sessions#destroy'
 
-  
+
+  resources :users
+  resources :microposts
 end
